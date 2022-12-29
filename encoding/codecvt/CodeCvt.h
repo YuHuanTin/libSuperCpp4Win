6f0e4107b59e215c9b1b89f2ee9c992c335f5101
23945 <2394555023@qq.com>
2022-12-29 18:59:17 +0800

    新建编码转换

---
+++ encoding/codecvt/CodeCvt.h
@@ -0,0 +1,21 @@
+#ifndef LIBSUPERCPP4WIN_CODECVT_H
+#define LIBSUPERCPP4WIN_CODECVT_H
+
+#include <string>
+#include <memory>
+#include "Windows.h"
+
+class CodeCvt {
+public:
+    //used for string
+    static std::string WstrToStr(const std::wstring &Src, UINT CodePage = CP_ACP);
+    static std::wstring StrToWstr(const std::string &Src, UINT CodePage = CP_ACP);
+
+    //used for char *
+    static std::unique_ptr<char[]> WstrToStr(wchar_t *Src, UINT CodePage = CP_ACP);
+    static std::unique_ptr<wchar_t[]> StrToWstr(char *Src, UINT CodePage = CP_ACP);
+};
+
+
+
+#endif //LIBSUPERCPP4WIN_CODECVT_H
